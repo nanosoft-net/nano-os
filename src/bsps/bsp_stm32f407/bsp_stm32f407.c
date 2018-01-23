accfaeff156eada8ab0afd9f2cb219d923a8c3a2
Cedric Jimenez <cjz@nano-soft.net>
2018-01-23 18:47:19 +0100

    Added support for Cortex- with MPU in STM32F407 BSP

--- src/bsps/bsp_stm32f407/bsp_stm32f407.c
+++ src/bsps/bsp_stm32f407/bsp_stm32f407.c
@@ -109,3 +109,25 @@ void NANO_OS_BSP_LedOff(const uint8_t led)
             break;
     }
 }
+
+/** \brief Get the LED I/O registers memory area description */
+void NANO_OS_BSP_GetLedIoRegistersMem(uint32_t* const start_address, uint32_t* const size)
+{
+    /* Check parameters */
+    if ((start_address != NULL) && (size != NULL))
+    {
+        (*start_address) = GPIOA_BASE;
+        (*size) = 0x3000u;
+    }
+}
+
+/** \brief Get the UART registers memory area description */
+void NANO_OS_BSP_GetUartIoRegistersMem(uint32_t* const start_address, uint32_t* const size)
+{
+    /* Check parameters */
+    if ((start_address != NULL) && (size != NULL))
+    {
+        (*start_address) = USART3_BASE;
+        (*size) = 0x0400u;
+    }
+}
